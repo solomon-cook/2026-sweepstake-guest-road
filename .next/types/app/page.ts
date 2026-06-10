1d9e85c3dac3fd5902ed75a52594c22286972fe4
Solomon Cook <solomon.j.cook@gmail.com>
2026-06-10 09:59:59 +0100

    Refine card pack reveal layout and styling

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/solomon/Documents/2026-sweepstake-guest-road/src/app/page.tsx
+// File: /Users/Solomon/Documents/2026-sweepstake-guest-road/src/app/page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
